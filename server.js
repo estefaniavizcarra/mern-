3e2d1eec1244b0089f27881254f7a79375799a8e
Estefanía Vizcarra <estefaniavizcarra@MacBook-Air-de-Estefania-2.local>
2019-07-17 21:50:21 -0500

    react reading list

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/books";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist";
 
 mongoose.connect(MONGODB_URI);
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 // Start the API server
 app.listen(PORT, function() {
